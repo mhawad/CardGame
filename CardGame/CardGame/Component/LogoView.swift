2f3763b4bf9ae55011ffaef2d64e86ccc1057e34
Mirna Sokar <mirnahelmybio@yahoo.com>
2022-04-21 14:00:47 -0400

    Add files via upload
    "adding xcoode file"

---
+++ CardGame/CardGame/Component/LogoView.swift
@@ -0,0 +1,20 @@
+//
+//  LogoView.swift
+//  CardGame
+//
+//  Created by Mirna Helmy on 4/21/22.
+//
+
+import SwiftUI
+
+struct LogoView: View {
+    var body: some View {
+        Image("Poker")
+    }
+}
+
+struct LogoView_Previews: PreviewProvider {
+    static var previews: some View {
+        LogoView()
+    }
+}
---
+++ CardGame/CardGame/Views/RegisterView.swift
@@ -0,0 +1,22 @@
+//
+//  RegisterView.swift
+//  CardGame
+//
+//  Created by Mirna Helmy on 4/21/22.
+//
+
+import SwiftUI
+
+struct RegisterView: View {
+    var body: some View {
+        ScrollView(showsIndicators: true) {
+            
+        }
+        }
+}
+
+struct RegisterView_Previews: PreviewProvider {
+    static var previews: some View {
+        RegisterView()
+    }
+}
