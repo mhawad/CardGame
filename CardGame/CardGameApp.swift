96b7600542b5a6379c7f5cc09e738ae06d84a83f
Mirna Helmy <mirna@Mirnas-iMac.local>
2022-04-21 13:59:27 -0400

    card game

---
+++ CardGame/CardGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardGameApp.swift
+//  CardGame
+//
+//  Created by Mirna Helmy on 4/21/22.
+//
+
+import SwiftUI
+
+@main
+struct CardGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CardGame/Views/LoginView.swift
@@ -0,0 +1,25 @@
+//
+//  LoginView.swift
+//  CardGame
+//
+//  Created by Mirna Helmy on 4/21/22.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        ScrollView(showsIndicators: true) {
+      
+        }
+        
+        
+        
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
